d360ada624c4ab434003d8d3fb8d3514da9062e7
Anton <anton@miresource.com>
2024-07-10 16:16:06 +0200

    Add linkedin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,6 +62,25 @@ export default function Home() {
           </video>
         </div>
       </div>
+      <div className="my-4">
+        <LinkedInLink />
+      </div>
     </main>
+  );
+}
+
+import { FaLinkedin } from 'react-icons/fa';
+
+function LinkedInLink() {
+  return (
+    <a
+      href="https://www.linkedin.com/in/anton-khomenko-front-dev/"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-blue-700 hover:text-blue-800 flex items-center"
+    >
+      <FaLinkedin size={24} />
+      <span className="ml-1">Connect with me on LinkedIn</span>
+    </a>
   );
 }
