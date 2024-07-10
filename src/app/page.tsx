b0aed4ba6616b3a9a4f3646149972183345b0d64
Anton <anton@miresource.com>
2024-07-10 12:27:30 +0200

    Add carusel with screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,38 @@
+import Image from 'next/image';
+// import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { CarouselComponent } from '@/app/shared/components/CarouselComponent';
+
 export default function Home() {
-  return <main className=""></main>;
+  const images = [
+    'https://framerusercontent.com/images/OJ6zrMzuB4HRZe1FDsotmybieo.jpg',
+    'https://framerusercontent.com/images/uFixGuWDGRnf39s5eL9F9zqxDhw.jpg',
+    'https://framerusercontent.com/images/Kbf4hsbDqGE5BSgzAp3K2W0TTo8.jpg',
+    'https://framerusercontent.com/images/ezl04ZMUJoal4godUSHKZ0CaU.jpg',
+    'https://framerusercontent.com/images/JoCvpvvWEgFWX0ofLO9ybQNTro.jpg',
+    'https://framerusercontent.com/images/tQ0y2i7UTaeRRipxD05QBNdGbk0.jpg',
+    'https://framerusercontent.com/images/ha2adl3YsEeo2l4p5mwlefUlQ.jpg',
+  ];
+
+  return (
+    <main className="min-h-screen flex flex-col items-center ">
+      <h1 className="text-4xl font-bold text-center my-14">Anton Khomenko</h1>
+
+      <div className="flex">
+        <div className="bg-[#BDBBB0] rounded-lg w-[50vw] relative overflow-hidden">
+          <Image
+            src="https://framerusercontent.com/images/xKEkFSWstXLWyVcQ1MccSwEy53g.jpg"
+            alt="screen"
+            objectFit="cover"
+            layout="fill"
+            className="rounded-lg"
+          />
+        </div>
+        <div className="bg-[#7E7A71] ml-1 rounded-lg w-[50vw] h-fit">
+          <div className="mx-auto w-[40vw] rounded-lg">
+            <CarouselComponent images={images} />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
---
+++ src/app/shared/components/CarouselComponent.tsx
@@ -0,0 +1,110 @@
+'use client';
+
+import Image from 'next/image';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+interface CarouselProps {
+  images: string[];
+}
+
+export function CarouselComponent({ images }: CarouselProps) {
+  return (
+    <Carousel
+      showThumbs={false}
+      autoPlay
+      showStatus={false}
+      // interval={5000}
+      // transitionTime={500}
+      dynamicHeight={false}
+      infiniteLoop
+      renderArrowPrev={(onClickHandler, hasPrev, label) =>
+        hasPrev && (
+          <button
+            type="button"
+            onClick={onClickHandler}
+            title={label}
+            className="absolute top-1/2 transform -translate-y-1/2 left-4 bg-gray-800 text-white p-2 rounded-full z-10 font-bold w-10"
+          >
+            ‹
+          </button>
+        )
+      }
+      renderArrowNext={(onClickHandler, hasNext, label) =>
+        hasNext && (
+          <button
+            type="button"
+            onClick={onClickHandler}
+            title={label}
+            className="absolute top-1/2 transform -translate-y-1/2 right-4 bg-gray-800 text-white p-2 rounded-full z-10 font-bold w-10"
+          >
+            ›
+          </button>
+        )
+      }
+    >
+      {images.map((item, index) => (
+        <div key={index}>
+          <Image
+            src={item}
+            alt={'screen'}
+            width={600}
+            height={400}
+            className="rounded-lg"
+          />
+        </div>
+      ))}
+    </Carousel>
+  );
+}
+
+//
+// import React, { useState } from 'react';
+//
+// export const CarouselComponent = ({ images }: CarouselProps) => {
+//   const [currentIndex, setCurrentIndex] = useState(0);
+//
+//   const nextSlide = () => {
+//     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+//   };
+//
+//   const prevSlide = () => {
+//     setCurrentIndex(
+//       (prevIndex) => (prevIndex - 1 + images.length) % images.length,
+//     );
+//   };
+//
+//   return (
+//     <div className="relative w-1/2 max-h-screen">
+//       <button
+//         onClick={prevSlide}
+//         className="absolute top-1/2 transform -translate-y-1/2 left-0 bg-gray-800 text-white p-2 rounded-full"
+//       >
+//         ‹
+//       </button>
+//       <div className="overflow-hidden">
+//         {images.map((image, index) => (
+//           <div
+//             key={index}
+//             className={`transition-opacity duration-500 ease-in-out ${index === currentIndex ? 'opacity-100' : 'opacity-0'}`}
+//             style={{ display: index === currentIndex ? 'block' : 'none' }}
+//           >
+//             <Image
+//               src={image}
+//               alt={'screen'}
+//               width={600}
+//               height={400}
+//               className="rounded-lg w-full h-auto max-h-screen"
+//             />
+//           </div>
+//         ))}
+//       </div>
+//       <button
+//         onClick={nextSlide}
+//         className="absolute top-1/2 transform -translate-y-1/2 right-0 bg-gray-800 text-white p-2 rounded-full"
+//       >
+//         ›
+//       </button>
+//     </div>
+//   );
+// };
